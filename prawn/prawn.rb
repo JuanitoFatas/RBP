a0ffe82de3c52adc3c9705e7d5252360f0568d5b
Hsin-Hsiang Huang <katehuang0320@gmail.com>
2013-07-05 22:50:34 +0800

    better implementation for prawn.rb

--- prawn/prawn.rb
+++ prawn/prawn.rb
@@ -5,22 +5,8 @@ require './test_unit_extensions'
 
 class Prawn::Document
   def parse_inline_styles(text)
-    sc = StringScanner.new(text)
-    output = []
-    last_pos = 0
-    loop do
-      if sc.scan_until(/<\/?[ib]>/)
-        pre = sc.pre_match[last_pos..-1]
-        output << pre unless pre.empty?
-        output << sc.matched
-        last_pos = sc.pos
-      else
-        output << sc.rest if sc.rest?
-        break output
-      end
-    end
-
-    output.length == 1 ? output.first : output
+    segments = text.split(%r{(</?.*?>)}).reject {|x| x.empty? }
+    segments.size == 1 ? segments.first : segments
   end
 end
 
