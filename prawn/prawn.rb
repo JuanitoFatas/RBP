aa131e4075e6689148c4ec153f47857fb4eef01e
Hsin-Hsiang Huang <katehuang0320@gmail.com>
2013-07-06 14:32:08 +0800

    refactor prawn

--- prawn/prawn.rb
+++ prawn/prawn.rb
@@ -1,8 +1,18 @@
 require 'prawn'
 
-class Prawn::Document
-  def parse_inline_styles(text)
-    segments = text.split(%r{(</?[ib]>)}).delete_if {|x| x.empty? }
-    segments.size == 1 ? segments.first : segments
+class Prawn::Document::Text
+
+  module StyleParser
+    extend self
+
+    TAG_PATTERN = %r{(</?[ib]>)}
+
+    def process(text)
+      (style_tag? text) ? text.split(TAG_PATTERN).delete_if {|x| x.empty? } : text
+    end
+
+    def style_tag?(text)
+      !!(text =~ TAG_PATTERN) ## !! is to conv match index to boolean.
+    end
   end
 end
--- prawn/test_prawn.rb
+++ prawn/test_prawn.rb
@@ -2,36 +2,45 @@ require './prawn'
 require 'test/unit'
 require './test_unit_extensions'
 
-
 class TestInlineStyleParsing < Test::Unit::TestCase
 
+  def setup
+    @parser = Prawn::Document::Text::StyleParser
+  end
+
   must "simply return the string if styles are not found" do
-    @pdf = Prawn::Document.new
-    assert_equal "Hello World", @pdf.parse_inline_styles("Hello World")
+
+    assert_equal "Hello World", @parser.process("Hello World")
   end
 
   must "parse italic tags" do
-    @pdf = Prawn::Document.new
+
     assert_equal ["Hello ", "<i>", "Fine", "</i>", " World"],
-                  @pdf.parse_inline_styles("Hello <i>Fine</i> World")
+                  @parser.process("Hello <i>Fine</i> World")
   end
 
   must "parse bold tags" do
-    @pdf = Prawn::Document.new
+
     assert_equal ["Some very ", "<b>", "bold text", "</b>"],
-                  @pdf.parse_inline_styles("Some very <b>bold text</b>")
+                  @parser.process("Some very <b>bold text</b>")
   end
 
   must "parse mixed italic and bold tags" do
-    @pdf = Prawn::Document.new
+
     assert_equal ["Hello ", "<i>", "Fine ", "<b>", "World", "</b>", "</i>"],
-                  @pdf.parse_inline_styles("Hello <i>Fine <b>World</b></i>")
+                  @parser.process("Hello <i>Fine <b>World</b></i>")
   end
 
   must "not split out other tags than <i>, <b>, </i>, </b>" do
-    @pdf = Prawn::Document.new
+
     assert_equal ["Hello <ruby>Ch", "<b>", "arl", "</b>", "ie</ruby>"],
-                  @pdf.parse_inline_styles("Hello <ruby>Ch<b>arl</b>ie</ruby>")
+                  @parser.process("Hello <ruby>Ch<b>arl</b>ie</ruby>")
+  end
+
+  must "be able to check whether a string needs to be parsed" do
+
+    assert ! @parser.style_tag?("Hello World")
+    assert   @parser.style_tag?("Hello <i>Fine</i> World")
   end
 
 end
